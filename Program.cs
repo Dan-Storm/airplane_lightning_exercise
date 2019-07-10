9ceeea75cff8aed41ea874a0c7f7a654f7f70c0c
Dan Storm <stormdanp@gmail.com>
2019-07-10 10:52:52 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace airplane_lightning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Boeing = new Plane()
+            {
+                Model = "737 Max",
+                Make = "Boeing",
+                Seats = 152,
+            };
+
+            Boeing.Fly();
+        }
+        class Plane
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Seats { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{Make}: Pretty fly for a white guy!");
+            
+        }
+    }
+    }
+}
